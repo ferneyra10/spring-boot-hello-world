d4b66adf13472c60494981bcce47c52809d0310a
ferney10 <76914924+ferneyra10@users.noreply.github.com>
2022-08-24 09:47:47 -0500

    Update HelloController.java

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-	@Value("${saludo}")
+	@value("${saludo}")
 	private String saludo;
 	
 	@GetMapping("/hello")
